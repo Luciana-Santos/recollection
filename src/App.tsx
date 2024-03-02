abfec6525ebf1e3c653e759d44bd4ede41e0cf81
Luciana Santos <github@luciana.dev>
2024-03-02 02:58:53 -0300

    refactor: 💡 Reinventing the wheel. Again
    Cleaning up the project boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+export default function App() {
+  return (
+    <main>
+      <h1>Work in progress...</h1>
+
+      <Button>Trying</Button>
+    </main>
+  )
+}
