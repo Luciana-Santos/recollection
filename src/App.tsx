d2b5c86660ff44486da05c6644eaf8b462d6b9ff
Luciana Santos <95719499+Luciana-Santos@users.noreply.github.com>
2024-12-26 21:45:09 -0300

    Feat supabase config (#5)
    * feat: 🎸 Pages skeleton added to the project
    
    * feat: 🎸 Context menu added to the file cards
    
    * feat: 🎸 Supabase basic configuration added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+})
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   )
 }
 
