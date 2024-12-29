491ee653874c24be50d7a8b40d2d58a23fe6035d
Luciana Santos <95719499+Luciana-Santos@users.noreply.github.com>
2024-12-29 20:05:26 -0300

    Feat/upload functionality (#10)
    * feat: 🎸 Pages skeleton added to the project
    
    * feat: 🎸 Context menu added to the file cards
    
    * feat: 🎸 File upload functionality added
    
    Now it's possible to upload files

--- src/services/apiFiles.ts
+++ src/services/apiFiles.ts
@@ -1,4 +1,4 @@
-import supabase from './supabase'
+import supabase, { supabaseUrl } from './supabase'
 
 export async function getFiles() {
   const { data: files, error } = await supabase.from('files').select('*')
@@ -26,7 +26,36 @@ export async function getFile(id: string | null) {
   return file
 }
 
-export async function deleteFile(id) {
+export async function uploadFile(newFile) {
+  const imageName = `${Math.random()}-${newFile.image.name}`.replaceAll('/', '')
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/recollection-images/${imageName}`
+
+  // ? Cria o arquivo
+  const { data, error } = await supabase
+    .from('files')
+    .insert([{ ...newFile, image: imagePath }])
+
+  if (error) {
+    console.error(error)
+    throw new Error('File could not be uploaded')
+  }
+
+  // ? Upload da imagem
+  const { error: storageError } = await supabase.storage
+    .from('recollection-images')
+    .upload(imageName, newFile.image)
+
+  // ? Deleta o arquivo se der erro no upload da imagem
+  if (storageError) {
+    await supabase.from('files').delete().eq('id', data.id)
+    console.error(storageError)
+    throw new Error('Image could not be loaded and the file was not uploaded')
+  }
+
+  return data
+}
+
+export async function deleteFile(id: string) {
   const { data, error } = await supabase.from('files').delete().eq('id', id)
 
   if (error) {
