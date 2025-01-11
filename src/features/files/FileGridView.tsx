10d00e43a1485f81bb06ffc3a24a2ae2d723e309
Luciana Santos <95719499+Luciana-Santos@users.noreply.github.com>
2025-01-11 00:32:10 -0300

    Feat/sort by (#26)
    * feat: 🎸 Pages skeleton added to the project
    
    * feat: 🎸 Context menu added to the file cards
    
    * feat: 🎸 Sort component basic structure and styles added
    
    * feat: 🎸 Sort by functionality added

--- src/features/files/FileGridView.tsx
+++ src/features/files/FileGridView.tsx
@@ -2,12 +2,14 @@ import FileGridViewSkeleton from '@/ui/FileGridViewSkeleton'
 import Card from '../../ui/FileCard'
 import EmptyGridView from '../../ui/EmptyGridView'
 import { useFiles } from './useFiles'
+import FilesSortOperations from './FilesSortOperations'
 
 type TagType = {
-  tag?: number
+  tag: number
+  showSortBy?: boolean
 }
 
-function FileGridView({ tag }: TagType) {
+function FileGridView({ tag, showSortBy = true }: TagType) {
   const { files, isLoading } = useFiles(tag)
 
   if (isLoading) return <FileGridViewSkeleton />
@@ -16,7 +18,8 @@ function FileGridView({ tag }: TagType) {
 
   if (files.length === 1) {
     return (
-      <div className="overflow-hidden min-h-screen  bg-gray-700 rounded-t-2xl">
+      <div className="overflow-hidden flex flex-col min-h-screen bg-gray-700 rounded-t-2xl">
+        {showSortBy && <FilesSortOperations />}
         <main className="main-content items-center">
           {files.map((file) => {
             return <Card key={file.id} data={file} className="max-w-[200px]" />
@@ -28,6 +31,8 @@ function FileGridView({ tag }: TagType) {
 
   return (
     <main className="overflow-hidden bg-gray-700 flex flex-col rounded-t-2xl">
+      {showSortBy && <FilesSortOperations />}
+
       <div className="main-content">
         {files.map((file) => {
           return <Card key={file.id} data={file} />
--- src/features/files/useFiles.ts
+++ src/features/files/useFiles.ts
@@ -1,17 +1,23 @@
 import { getFiles } from '@/services/apiFiles'
 import { ICardData } from '@/types/types'
 import { useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
 
-export function useFiles(tag?: number) {
-  // const queryClient = useQueryClient()
+export function useFiles(tag: number) {
+  const [searchParams] = useSearchParams()
+
+  // SORT
+  const sortByRaw = searchParams.get('sortBy') || 'created_at-desc'
+  const [field, direction] = sortByRaw.split('-')
+  const sortBy = { field, direction }
 
   const {
     data: files = [],
     isLoading,
     error,
   } = useQuery<ICardData[]>({
-    queryKey: ['files'],
-    queryFn: () => getFiles(tag),
+    queryKey: ['files', sortBy],
+    queryFn: () => getFiles({ tag, sortBy }),
   })
 
   return { isLoading, files, error }
