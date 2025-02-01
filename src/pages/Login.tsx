0a01e7f1c9b4394a2082377b5c58f4c332b6aed7
Luciana Santos <github@luciana.dev>
2025-02-01 17:35:17 -0300

    feat: 🎸 Adding Google login functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import GoogleIcon from '../ui/GoogleIcon'
+import SignIn from '@/features/auth/SignIn'
 import Logo from '../ui/Logo'
 
 function Login() {
@@ -7,10 +7,7 @@ function Login() {
       <div className="py-20 bg-gray-700 rounded-2xl px-10 sm:px-24 gap-12 grid justify-center text-center">
         <Logo showDescription className="flex-col items-center" />
 
-        <button className="flex gap-2 border hover:border-secondary rounded-sm py-2 px-3 items-center justify-center text-gray-300 border-gray-300 hover:text-secondary">
-          <GoogleIcon />
-          Sign in with Google
-        </button>
+        <SignIn />
       </div>
     </main>
   )
