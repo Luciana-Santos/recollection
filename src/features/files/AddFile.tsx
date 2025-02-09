7cc21f9b730306fc4bcb08ca81e4e95ba407847a
Luciana Santos <github@luciana.dev>
2025-02-09 02:04:17 -0300

    feat: 🎸 Form submit button now has a loader when uploading
    ✅ Closes: #31

--- src/features/files/AddFile.tsx
+++ src/features/files/AddFile.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { ImagePlus } from 'lucide-react'
+import { ImagePlus, LoaderCircle } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { formSchema, FormSchemaType } from './formSchema'
 import { useUploadFile } from './useUploadFile'
@@ -224,15 +224,20 @@ function AddFile() {
         </section>
 
         <div className="flex gap-5 justify-end">
-          <button type="reset" className="text-gray-300 hover:underline">
+          <button
+            disabled={isWorking}
+            type="reset"
+            className="text-gray-300 hover:underline"
+          >
             Cancel
           </button>
           <button
             disabled={isWorking}
             type="submit"
-            className="w-[120px] main-action"
+            className="w-[120px] main-action flex gap-2 items-center justify-center"
           >
-            {isEditSession ? 'Update' : 'Upload'}
+            {!isWorking && (isEditSession ? 'Update' : 'Upload')}
+            {isWorking && <LoaderCircle className="animate-spin" />}
           </button>
         </div>
       </form>
